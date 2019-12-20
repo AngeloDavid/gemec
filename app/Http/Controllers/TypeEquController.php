876eebf7f9bc9b459add56b29cab5229373eb129
AuxSistemas <AngeloDavid95>
2019-12-20 18:37:25 -0500

    creacion de modelo de tipo de equipo

---
+++ app/Http/Controllers/TypeEquController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TypeEqu;
+use Illuminate\Http\Request;
+
+class TypeEquController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TypeEqu  $typeEqu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TypeEqu $typeEqu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TypeEqu  $typeEqu
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TypeEqu $typeEqu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TypeEqu  $typeEqu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TypeEqu $typeEqu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TypeEqu  $typeEqu
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TypeEqu $typeEqu)
+    {
+        //
+    }
+}
---
+++ app/TypeEqu.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TypeEqu extends Model
+{
+    //
+    public $table="TypeEqu";
+    protected $fillable =[
+        'name'
+    ] ;
+
+}
